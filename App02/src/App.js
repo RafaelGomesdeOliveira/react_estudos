8de18c3a20ea3a5153ca72615b73af9e89e55800
RafaelGomesdeOliveira <rafaelgoliveiraa@gmail.com>
2024-12-24 18:26:43 -0300

    Reorganizei a minha estrutura de pastas

---
+++ App02/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Relogio from "./components/Relogio";
+import Header from "./components/Header";
+
+export default function App() {
+  return( 
+  <>
+  <Header/>
+  Aqui
+    <Relogio/>
+  </>
+  )
+}
---
+++ App02/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";  
+
+
+export default function Header() {
+    return (
+        <>
+            <h1>Título teste</h1>   
+        </>
+    )
+}
